7268afb9d2d46e08c8e11a712decb12ccb7685a2
jwells89 <john.ivan@me.com>
2015-02-04 14:22:52 -0800

    Initial Commit

---
+++ AtlassianDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AtlassianDemo
+//
+//  Created by John Wells on 2/4/15.
+//  Copyright (c) 2015 John Wells. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ AtlassianDemo/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  AtlassianDemo
+//
+//  Created by John Wells on 2/4/15.
+//  Copyright (c) 2015 John Wells. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
